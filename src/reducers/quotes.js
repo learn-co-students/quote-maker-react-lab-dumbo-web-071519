12cbe0a8ab808bf2e8de831f8459bc962d8820be
jason <jasonedlewis@gmail.com>
2019-10-07 01:21:44 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,32 @@
+
+
 export default (state = [], action) => {
-  return state;
+
+    switch (action.type) {
+      case "ADD_QUOTE":
+        return [ action.quote, ...state]
+        break;
+      case "REMOVE_QUOTE":
+        const remaingQuotes = state.filter(quote => quote.id !== action.quoteId)
+        return [remaingQuotes]
+      case "UPVOTE_QUOTE":
+       return {
+         ...state,
+         vote: state.vote + 1
+       }
+       case "DOWNVOTE_QUOTE":
+         let idx =  state.findIndex(quote => quote.id === action.quoteId)
+       return {
+         
+       }
+       return state;
+      default:
+        return state
+       
+    }
+
+
+
+
+
 }
